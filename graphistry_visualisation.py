a44b67500739a9076f068f6ec28325e7f2083572
lukasmarshall <lukasmarshall@gmail.com>
2017-09-05 22:14:56 +1200

    Taking out random print

--- graphistry_visualisation.py
+++ graphistry_visualisation.py
@@ -1,7 +1,7 @@
 import graphistry
 import networkGraph
 import networkx 
-print graphistry
+
 graphistry.register(key='555194151a1e7dd59b00ae0c4826d91b3bfbf4afac2239e05ec7180aa6f06e152d43ddea14a3616121b451cf00d150c1')
 
 
